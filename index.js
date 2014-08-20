6e627132df1a4d86459498409cff0ee324fce7b0
Greg Reimer <gregreimer@gmail.com>
2014-08-20 11:26:08 -0600

    Handle both streaming and non-streaming body send.
    If the request body has been parsed and you have this.request.body
    available, then send it. Otherwise stream this.req using a pipe();
    
    Also added tests for these use cases.

--- index.js
+++ index.js
@@ -19,16 +19,25 @@ module.exports = function(options) {
       return yield* next;
     }
 
-    var body = mungeBody(this.request.body, this.request.header['content-type']);
+    var parsedBody = getParsedBody(this);
 
     var opt = {
       url: url + '?' + this.querystring,
       headers: this.header,
       encoding: null,
       method: this.method,
-      body: body
+      body: parsedBody
     };
-    var res = yield request(opt);
+
+    var requestThunk = request(opt);
+
+    if (parsedBody) {
+      var res = yield requestThunk;
+    } else {
+      // Is there a better way?
+      // https://github.com/leukhin/co-request/issues/11
+      var res = yield pipeRequest(this.req, requestThunk);
+    }
 
     this.status = res.statusCode;
     for (var name in res.headers) {
@@ -65,16 +74,24 @@ function ignoreQuery(url) {
   return url ? url.split('?')[0] : null;
 }
 
-function mungeBody(body, contentType){
+function getParsedBody(ctx){
+  var body = ctx.request.body;
   if (body === undefined || body === null){
-    return body;
+    return undefined;
   }
+  var contentType = ctx.request.header['content-type'];
   if (!Buffer.isBuffer(body) && typeof body !== 'string'){
     if (contentType.indexOf('json') !== -1){
       body = JSON.stringify(body);
     } else {
-      body = body + ''; // coerce it to a string
+      body = body + '';
     }
   }
   return body;
+}
+
+function pipeRequest(readable, requestThunk){
+  return function(cb){
+    readable.pipe(requestThunk(cb));
+  }
 }
